06061d52137eb8510cf00a7fb0e5651ffa260589
danielrluna <daniel.luna.088@ufrn.edu.br>
2024-07-03 16:14:34 -0300

    new campaign.sh

--- campaign.sh
+++ campaign.sh
@@ -2,7 +2,7 @@
 
 # Usage message
 usage() {
-  echo "Usage: $0 -j num_jobs -c cpus -d distance -C capacity -o odc"
+  echo "Usage: $0 -j num_jobs -c cpus -d distance -C capacity -o odc -n campaign_name"
   exit 1
 }
 
@@ -12,9 +12,10 @@ cpus=0
 distance=0
 capacity=0
 odc=0
+campaign_name=""
 
 # Parse command line arguments
-while getopts ":j:c:d:C:o:" opt; do
+while getopts ":j:c:d:C:o:n:" opt; do
   case ${opt} in
     j)
       num_jobs=$OPTARG
@@ -31,6 +32,9 @@ while getopts ":j:c:d:C:o:" opt; do
     o)
       odc=$OPTARG
       ;;
+    n)
+      campaign_name=$OPTARG
+      ;;
     \?)
       usage
       ;;
@@ -38,18 +42,22 @@ while getopts ":j:c:d:C:o:" opt; do
 done
 
 # Check if all required parameters are provided
-if [ $num_jobs -eq 0 ] || [ $cpus -eq 0 ] || [ $distance -eq 0 ] || [ $capacity -eq 0 ] || [ $odc -eq -1 ]; then
+if [ $num_jobs -eq 0 ] || [ $cpus -eq 0 ] || [ $distance -eq 0 ] || [ $capacity -eq 0 ] || [ $odc -eq 0 ] || [ -z "$campaign_name" ]; then
   usage
 fi
 
 # Base directory for the jobs
 base_dir="/home/ubuntu/EmbrapiiCPqD/OpenRanDatacenterPlacement/open_ran_datacenter_placement"
+campaign_dir="${base_dir}/Campaign_${campaign_name}"
+
+# Create campaign directory
+mkdir -p "${campaign_dir}"
 
 # Loop to create Job directories and run the script
 for ((i=1; i<=num_jobs; i++))
 do
-  job_dir="${base_dir}/Job${i}"
-  output_file="${job_dir}/Sim_${i}.out"
+  job_dir="${campaign_dir}/Job${i}"
+  output_file="${campaign_dir}/Sim_${i}.out"
   mkdir -p "${job_dir}"
   python3 odc_placement_parser.py -c=${cpus} -d=${distance} -cp=${capacity} -t=60 -pop=300 -p=8 -o=${odc} -wcpu=0.4 -wodc=0.4 -wd=0.2 -s=$i -opd="${job_dir}" > "${output_file}" 2>&1
 done
